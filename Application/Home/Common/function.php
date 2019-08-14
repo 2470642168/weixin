8c73f9fa486ce22b4f2cd1ef53c8160833ae0911
admin <2470642168@qq.com>
2019-08-14 19:10:45 +0800

    3

--- Application/Home/Common/function.php
+++ Application/Home/Common/function.php
@@ -1,6 +1,6 @@
 <?php
-	if (!function_exists('http_curl')) {
-		function http_curl($url, $method = 'get' , $postdata = array()){
+	if(!function_exists('http_curl')){
+		function http_curl($url,$method='get',$postdata=array()){
 			// 1.初始化
 			$ch = curl_init();
 			// 2.设置参数
@@ -13,9 +13,9 @@
 			//get方式
 			curl_setopt($ch, CURLOPT_URL, $url);
 			//结果返回 不直接输出
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER,TRUE);
 			//禁用 https 证书检查
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER ,FALSE);
 			// 3.发送请求
 			$json = curl_exec($ch);
 			// 4.关闭

dc23480adc2eb0dbff561cd0f4d78af7fcfb386d
admin <2470642168@qq.com>
2019-08-14 18:10:17 +0800

    1

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -2,10 +2,11 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
-    public function index(){
-        $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" . C('appid') . "&secret=" . C('appsecret');
+    // public function index(){
+    //     $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" . C('appid') . "&secret=" . C('appsecret');
 
-        $json = http_curl($url);
-        dump($json);
-    }
+    //     $json = http_curl($url);
+    //     dump($json);
+    // }
+    require 'index.html';
 }

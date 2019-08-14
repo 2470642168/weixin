6648df591fc06d4ed9825b8338b7b3a0e91c244e
admin <2470642168@qq.com>
2019-08-14 18:04:48 +0800

    测试url发送请求获取公众号账号消息

---
+++ Application/Common/Common/function.php
@@ -0,0 +1,24 @@
+<?php
+	if (!function_exists('http_curl')) {
+		function http_curl($url, $method = 'get' , $postdata = array()){
+			// 1.初始化
+			$ch = curl_init();
+			// 2.设置参数
+			if ($method == 'post'){
+				//post方式
+				curl_setopt($ch, CURLOPT_POST, TRUE);
+				//参数
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+			}elseif ($method == 'get') {
+				//get方式
+				curl_setopt($ch, CURLOPT_URL, $url);
+				//结果返回 不直接输出
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+			}
+			// 3.发送请求
+			$json = curl_exec($ch);
+			// 4.关闭
+			curl_close($ch);
+			return $json;
+		}
+	}
--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,4 +1,8 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	
+	//微信公众号平台的身份信息
+	'appid' => 'wxb2478e9e6a676496',
+	'appsecret' => '207c94be38a6c5780d8beb8b5e2fc770'
 );

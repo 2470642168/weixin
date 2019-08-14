5ea44af43efb5749a5a4a3973e7c790472442235
admin <2470642168@qq.com>
2019-08-14 18:33:36 +0800

    2

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -4,7 +4,7 @@ use Think\Controller;
 class IndexController extends Controller {
      public function index(){
          $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" . C('appid') . "&secret=" . C('appsecret');
-echo 123;
+         
          $json = http_curl($url);
          dump($url);die;
      }
